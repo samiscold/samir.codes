047dfd96fab108bec3dcac0d4e4bbac1ca4bebe5
samiscold <62956581+samiscold@users.noreply.github.com>
2022-01-25 21:06:41 +0100

    get input from user

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,12 +1,26 @@
 // if () {}
 const pjat = document.querySelectorAll('p') // i merr i shtin array
-const inputi = document.querySelector('input').value;
 
+function myFunction() {
+    let inputi = document.getElementById('result').value;
+    console.log(inputi);
 
+ 
 
-if (inputi === "color a")
-{
+var abc = document.getElementById("wronginput");
+if (inputi === "color a"){
 for (let i = 0; i < pjat.length; i++) {
     pjat[i].style.color = 'lime' // ja qet krejtve emrin 1 ka 1 loop
+}}
+
+if (inputi === "cls"){
+    for (let i = 0; i < pjat.length; i++) {
+        pjat[i].style.color = 'lime' // ja qet krejtve emrin 1 ka 1 loop
+    }}
+
+
+else {
+    abc.innerHTML += "'" + inputi + "' is not recognized as an internal <br> or external command, p.s try <br>";
 }
+
 }
